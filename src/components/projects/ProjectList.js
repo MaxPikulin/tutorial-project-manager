46d8e31c04b9363beed5d4e9a115bd37a667c466
Max Pikulin <trurl1000@gmail.com>
2019-02-22 13:17:57 +0000

    projectDetails

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,32 +1,12 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
+const ProjectList = (props) => {
   return (
     <div className="project-list section">
-    
-      <div className="card z-depth0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Max Pikulin</p>
-          <p className="grey-text">22nd February, 12:29</p>
-        </div>
-      </div>
-      
-      <div className="card z-depth0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Max Pikulin</p>
-          <p className="grey-text">22nd February, 12:29</p>
-        </div>
-      </div>
-      
-      <div className="card z-depth0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Max Pikulin</p>
-          <p className="grey-text">22nd February, 12:29</p>
-        </div>
-      </div>
+    {['', '', '', ''].map((project, i) => {
+      return <ProjectSummary data={project} />
+    })}
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = (props) => {
+  return (
+    <div className="card z-depth0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by Max Pikulin</p>
+        <p className="grey-text">22nd February, 12:29</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
